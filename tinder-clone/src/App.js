159ec1dca0e4b7eeae56d8e3da0014b37789912c
saiviswateja <saiviswateja.teja@gmail.com>
2020-10-29 16:50:50 +0530

    partial complete of backend

--- tinder-clone/src/App.js
+++ tinder-clone/src/App.js
@@ -3,12 +3,7 @@ import './App.css';
 import Header from './Header';
 import TinderCards from './TinderCards';
 import SwipeButtons from './SwipeButtons';
-import ReplyIcon from '@material-ui/icons/Replay';
-import CloseIcon from '@material-ui/icons/Close';
-import StarRateIcon from '@material-ui/icons/StarRate';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import FlashIcon from '@material-ui/icons/FlashOn';
-import IconButton from '@material-ui/core/IconButton';
+
 
 function App() {
   return ( 
